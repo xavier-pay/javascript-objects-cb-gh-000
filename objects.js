9e8bfe11ca1325f471248613326bda6903896ccf
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-02-07 01:07:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,10 +4,14 @@ arafat: '202'
 }
 
 // Access Object
+updatePlaylist(playlist, artistName, songTitle){
+  playlist['slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
 
+  playlist.assign(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+}
 
 // Add a key-value pair
 
 
 // Delete a key-value pair
-
