2782554d1255c29b361bbe07fc1bff2bbe3e70ab
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-02-07 01:01:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,13 @@
 var person = {
-  firstname: 'xavier'
+  firstname: 'xavier',
+  lastname: 'aurelien'
 }
 
-person.assign(lastname:'payong')
+// Access Object
+  person.firstname
+  
+// Add a key-value pair
+  person.assign(age:'12')
+
+// Delete a key-value pair
+delete person.lastname
