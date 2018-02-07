66de56a933a2a0335e42404cbaa75ff0bcc283b4
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-02-07 01:02:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
-var person = {
-  firstname: 'xavier',
-  lastname: 'aurelien'
+var playlist = {
+arafat: '202'
+
 }
 
 // Access Object
-  person.firstname
-  
+
+
 // Add a key-value pair
-  person.assign(age:'12')
+
 
 // Delete a key-value pair
-delete person.lastname
+
